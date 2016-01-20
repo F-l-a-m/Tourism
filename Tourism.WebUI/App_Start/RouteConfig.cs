0eee0cc3c27ff34b6c7eaa73a757a1af23600d64
Andrew <andrew.flam@gmail.com>
2016-01-20 10:49:54 +0200

    Simple view of list - configured

--- Tourism.WebUI/App_Start/RouteConfig.cs
+++ Tourism.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tourism.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Tour", action = "List", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Tourism.WebUI/Controllers/TourController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tourism.Domain.Abstract;
+using Tourism.Domain.Entities;
+
+namespace Tourism.WebUI.Controllers
+{
+    public class TourController : Controller
+    {
+        private ITourRepository repository;
+        public TourController(ITourRepository tourRepository)
+        {
+            this.repository = tourRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Tours);
+        }
+    }
+}
