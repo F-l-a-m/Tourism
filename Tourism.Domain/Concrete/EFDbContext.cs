64aaa2011e54d3cb31c89ce8af2fa98cf7703a86
Andrew <andrew.flam@gmail.com>
2016-01-20 12:25:21 +0200

    Added database

---
+++ Tourism.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Tourism.Domain.Entities;
+using System.Data.Entity;
+
+namespace Tourism.Domain.Concrete {
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Tour> Tours { get; set; }
+    }
+}
---
+++ Tourism.Domain/Concrete/EFTourRepository.cs
@@ -0,0 +1,15 @@
+﻿using Tourism.Domain.Abstract;
+using Tourism.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Tourism.Domain.Concrete
+{
+    public class EFTourRepository : ITourRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IEnumerable<Tour> Tours
+        {
+            get { return context.Tours; }
+        }
+    }
+}
